f02c275484b92bb51c0b681c85860ca92cb5da9f
salah330 <salahsamir11111@gmail.com>
2020-05-19 12:50:43 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <h2>hello </h2>;
 }
 
 export default App;
